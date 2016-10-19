ca20d00dc7cbc77932e9d71e4571015f355b5385
jared weinstein <jaredweinstein1@gmail.com>
2016-10-19 01:37:15 -0400

    .

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_entry():
 		if net_id == None or time == None:
 			return make_response(jsonify({'error': 'net_id or time not provided'}), 404) 
 		print "NET ID: {} : {}".format(net_id, str(time))
-		cur.execute("INSERT INTO leaderboard (net_id, time) VALUES ({}, {});".format(net_id, time))
+		cur.execute("INSERT INTO leaderboard (net_id, time) VALUES ('{}', {});".format(net_id, time))
 		conn.commit()
 		return make_response(jsonify({'success': 'entry added'}), 404)
 	return 'hi'
